3ebf4cdc026c52ce8e884ca4013c3504b68d9bb6
jason3manley <jmanley_310@live.com>
2014-11-21 12:20:52 -0400

    Lottery game project

---
+++ KeyViewer.java
@@ -0,0 +1,50 @@
+// Laying Out an Application - LottoMadness part 2
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class KeyViewer extends JFrame implements KeyListener {
+     JTextField keyText = new JTextField(80);
+     JLabel keyLabel = new JLabel("Press any key in the text field.");
+
+     KeyViewer() {
+         super("KeyViewer");
+         setLookAndFeel();
+         setSize(350, 100);
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         keyText.addKeyListener(this);
+         BorderLayout bord = new BorderLayout();
+         setLayout(bord);
+         add(keyLabel, BorderLayout.NORTH);
+         add(keyText, BorderLayout.CENTER);
+         setVisible(true);
+     }
+//This tells you what keys you are pressing at the top of box
+     public void keyTyped(KeyEvent input) {
+         char key = input.getKeyChar();
+         keyLabel.setText("You pressed " + key);
+     }
+
+     public void keyPressed(KeyEvent txt) {
+         // do nothing 
+     }
+
+     public void keyReleased(KeyEvent txt) {
+         // do nothing 
+     }
+
+     private void setLookAndFeel() {
+         try {
+             UIManager.setLookAndFeel(
+                 "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel" 
+                 );
+             } catch (Exception exc) {
+                 // ignore error 
+             }
+     }
+
+     public static void main(String[] arguments) {
+         KeyViewer frame = new KeyViewer();
+     }
+  }
